12372bbbc840e64765ef1f172a408515f9db9890
Brent <brent@elegantmedia.com.au>
2022-02-07 15:16:21 +1100

    Initial Commit

---
+++ LearningGithub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGithub
+//
+//  Created by Brent on 7/2/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGithub/LearningGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGithubApp.swift
+//  LearningGithub
+//
+//  Created by Brent on 7/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
